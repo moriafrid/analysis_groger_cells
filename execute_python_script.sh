dafb8877b51e8abbeb979267a9ce20970bb95199
moriafrid <moria.fridman100@gmail.com>
2022-03-27 17:08:51 +0300

    just change the node to run

--- execute_python_script.sh
+++ execute_python_script.sh
@@ -9,7 +9,7 @@
 #SBATCH --mem 4GB
 ##SBATCH -c 1
 ##SBATCH -t 1-0
-#SBATCH --exclude=ielsc-48,ielsc-49,ielsc-42,ielsc-43
+#SBATCH --exclude=ielsc-48,ielsc-49,ielsc-44,ielsc-45
 
 echo "the number parameters the sbatch get is "$#
 
