2afac90ac9d6b94cc1e9760297f25eff1e80496d
moriafrid <moria.fridman100@gmail.com>
2022-03-28 11:30:19 +0300

    run_execute_main

--- run_execute_main.py
+++ run_execute_main.py
@@ -1,10 +1,20 @@
 import os
+from open_pickle import read_from_pickle
+import sys
+if len(sys.argv) != 3:
+    folder_=""
+    # folder_="/ems/elsc-labs/segev-i/moria.fridman/project/analysis_groger_cells/"
+    cells_name_place="cells_name.p"
+    print("run_execute not running with sys.argv",len(sys.argv))
+else:
+    folder_=sys.argv[1]
+    cells=sys.argv[2]
+    print("run_execute running with sys.argv",sys.argv)
 
-folder_="/ems/elsc-labs/segev-i/moria.fridman/project/analysis_groger_cells/"
+print("Remaind to choose the right syn")
 folder_data="cells_initial_information"
 folder_save="cells_outputs_data"
-cells=["2017_05_08_A_5-4", "2017_05_08_A_4-5","2017_03_04_A_6-7"]
-
+cells=read_from_pickle(cells_name_place)
 for cell_name in cells:
     command="sbatch execute_main.sh"
     send_command = " ".join([command, cell_name,folder_, folder_data , folder_save])
