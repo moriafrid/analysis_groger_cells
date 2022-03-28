2160f152687ca3d18b4f363f701d80d907e10a1c
moriafrid <moria.fridman100@gmail.com>
2022-03-28 11:30:04 +0300

    run_execute_main

--- run_all.py
+++ run_all.py
@@ -1,5 +1,9 @@
 import os
-os.system("python run_excute_main.py")
+cells_name_place='cells_name.p'
+folder=""
+commet_main=" ".join(["python run_excute_main.py", folder, cells_name_place ])
+os.system(commet_main)
+
 os.system("python run_execute_level1.py")
 os.system("python run_execute_fits.py")
 os.syatem("csv_for_passive_val_results.py")
