9549768cd87cf0d79a017beb2d43274315c7d269
moriafrid <moria.fridman100@gmail.com>
2022-03-29 15:20:38 +0300

    start working on short_analysis test

--- run_execute_level2.py
+++ run_execute_level2.py
@@ -5,7 +5,7 @@ from read_passive_parameters_csv import get_passive_parameter
 
 folder_="/ems/elsc-labs/segev-i/moria.fridman/project/analysis_groger_cells/"
 folder_data="cells_initial_information/"
-folder_save="cells_outputs_data/"
+folder_save="cells_outputs_data_short/"
 cells=["2017_05_08_A_5-4", "2017_05_08_A_4-5","2017_03_04_A_6-7"]
 # file_type2read=['hoc','ASC']
 file_type2read=['z_correct.swc','morphology.swc','hoc','ASC']
