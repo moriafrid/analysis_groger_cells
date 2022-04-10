c778327f3d912f8481b7cdd83b42627835473f13
moriafrid <moria.fridman100@gmail.com>
2022-04-10 11:18:21 +0300

    add correctopn for [1.0,1.0]

--- read_passive_parameters_csv.py
+++ read_passive_parameters_csv.py
@@ -7,10 +7,11 @@ def get_passive_parameter(cell_name,double_spine_area='False',shrinkage_resize=[
     #creat the value to run on
     df.loc[(df["fit_condition"] == fit_condition) & (df["file_type"] == file_type), :].to_dict('records')
 
-    if double_spine_area:
+    if eval(double_spine_area):
         curr = df.loc[(df["fit_condition"] == fit_condition) & (df["file_type"] == file_type)&(df["spine_start"] == spine_start)&(df['shrinkage&resize_factors']==str(shrinkage_resize))&(df['double_spine_area']==eval(double_spine_area)), ]
     else:
         curr = df.loc[(df["fit_condition"] == fit_condition) & (df["file_type"] == file_type)&(df["spine_start"] == spine_start)&(df['shrinkage&resize_factors']==str(shrinkage_resize)), ]
+
     if passive_param_name is not None:
         return curr.loc[df.parameter_type == passive_param_name,:].to_dict('records')[0]
 
