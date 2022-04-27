139b3a09c8f968a1bad837657067e454238c3a89
moriafrid <moria.fridman100@gmail.com>
2022-04-27 15:49:11 +0300

    correct the spine radius to be rigth insted meann on th eradius

--- read_spine_properties.py
+++ read_spine_properties.py
@@ -22,23 +22,30 @@ def get_parameter(cell_name,par_name,spine_num=None):
             print(par_name +" in "+cell_name+ "is empty at Data2.xlx")
     return par
 
-def get_R_head(parameter_cv):
+def get_R_head(cell_name,i=None):
+    df = pd.read_excel('cells_initial_information/Data2.xlsx')
+    parameter_cv=df[df['cell_name']==cell_name].reset_index()
     if not True in list(pd.isna(parameter_cv['R_head'])):
-        R_head=np.mean(parameter_cv['R_head'])
+        R_head=parameter_cv['R_head']
     elif not True in list(pd.isna(parameter_cv['head_diam'])):
-        R_head=np.mean(parameter_cv['head_diam'])/2
+        R_head=parameter_cv['head_diam']/2
     elif not True in list(pd.isna(parameter_cv['V_head'])):
-        V_head=np.mean(parameter_cv['V_head'])
+        V_head=parameter_cv['V_head']
         R_head = (V_head/(4*pi/3))**(1/3) #µm
     elif not True in list(pd.isna(parameter_cv['head_area'])):
-        head_area=np.mean(parameter_cv['head_area'])
-        R_head=sqrt(head_area/(4*pi))
-    return R_head
+        head_area=parameter_cv['head_area']
+        R_head=np.sqrt(head_area/(4*pi))
+    if i==None:
+        return np.mean(R_head)
+    elif i=='list':
+        return R_head
+    else:
+        return R_head[i]
 
-def get_F_factor_params(spin_type):
+def get_F_factor_params(spine_type,i=0):
     df = pd.read_excel('cells_initial_information/Data2.xlsx')
-    parameter_cv=df[df['cell_name']==spin_type].reset_index()
-    R_head=get_R_head(parameter_cv)
+    parameter_cv=df[df['cell_name']==spine_type].reset_index()
+    R_head=get_R_head(spine_type)
     neck_diam=np.mean(parameter_cv['neck_diam'])
     neck_length=np.mean(parameter_cv['neck_diam'])
     spine_density=np.mean(parameter_cv['spine_density'])
@@ -62,7 +69,7 @@ def get_spine_part(cell_name,spine_num):
 def get_building_spine(cell_name,spine_num):
     df = pd.read_excel('cells_initial_information/Data2.xlsx')
     parameter_cv=df[df['cell_name']==cell_name].reset_index()
-    return {'NECK_LENGHT':parameter_cv['neck_length'][spine_num],'NECK_DIAM':parameter_cv['neck_diam'][spine_num],'HEAD_DIAM':get_R_head(parameter_cv)*2}
+    return {'NECK_LENGHT':parameter_cv['neck_length'][spine_num],'NECK_DIAM':parameter_cv['neck_diam'][spine_num],'HEAD_DIAM':get_R_head(cell_name,spine_num)*2}
 
 def get_spine_params(spine_type,cell_name=''):
     # 'groger_spine' 'mouse_spine','human_spine','shaft_spine'
@@ -71,7 +78,7 @@ def get_spine_params(spine_type,cell_name=''):
     else:
         df = pd.read_excel('cells_initial_information/Data2.xlsx')
         parameter_cv=df[df['cell_name']==spine_type].reset_index()
-        return parameter_cv['neck_length'],parameter_cv['neck_diam'],get_R_head(parameter_cv)*2
+        return parameter_cv['neck_length'],parameter_cv['neck_diam'],get_R_head(cell_name,num='list')*2
 def get_sec_and_seg(cell_name,spine_num=None):
     df = pd.read_excel('cells_outputs_data_short/'+cell_name+'/synaptic_location_seperate.xlsx',index_col=0)
     if not spine_num is None:
@@ -86,5 +93,6 @@ if __name__ == '__main__':
     cell_name='2017_03_04_A_6-7'
     get_parameter(cell_name,'PSD')
     get_F_factor_params('human_spine')
+    get_R_head(cell_name,i=0)
     get_spine_xyz('2017_05_08_A_5-4',0)
     # get_psd(cell_name)
