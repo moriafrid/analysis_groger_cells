b82f5b7dcc43997efd5fbf9ed32ac5d940daa178
moriafrid <moria.fridman100@gmail.com>
2022-04-27 15:48:43 +0300

    not imporatnt changes

--- run_Moo_get_parameters.py
+++ run_Moo_get_parameters.py
@@ -22,16 +22,20 @@ folder_save="cells_outputs_data_short/"
 
 file_types=['z_correct.swc','morphology.swc','ASC']
 SPINE_STARTs=[60]
-for cell_name in read_from_pickle(cells_name_place)[1:2]:
+for cell_name in read_from_pickle(cells_name_place)[0:1]:
     passive_vals_dict= {}
     # p='cells_initiall_information/'+cell_name+'/results_passive_fits.csv'
     p='cells_outputs_data_short/'+cell_name+'/fit_short_pulse/results_passive_fits.csv'
     df = pd.read_csv(p)
-    for resize_diam_by ,shrinkage_by in zip([1.0,1.1,1.2,1.5][:1],[1.0,1.1,1.0,1.0][:1]):#zip([1.0],[1.0]):
+    for resize_diam_by ,shrinkage_by in zip([1.0,1.1,1.2,1.5][:3],[1.0,1.1,1.0,1.0][:3]):#zip([1.0],[1.0]):
         for fit_condition in ['const_param','different_initial_conditions'][:1]:
             for file_type in file_types:
                 for SPINE_START in [20,60,10][:1]:
-                    for double_spine_area in ['True','False'][1:]:
+                    if resize_diam_by==1.0 and shrinkage_by==1.0:
+                        do_double_spine_area=['True','False']
+                    else:
+                        do_double_spine_area=['False']
+                    for double_spine_area in do_double_spine_area:
                         passive_vals_dict=get_passive_parameter(cell_name,double_spine_area=double_spine_area,shrinkage_resize=[shrinkage_by,resize_diam_by],fit_condition=fit_condition,spine_start=SPINE_START,file_type=file_type)
                         for i, passive_val_name in enumerate(['RA=120','RA=150','RA_min_error','RA_best_fit']):
                             # if i!=2: continue
@@ -56,18 +60,3 @@ for cell_name in read_from_pickle(cells_name_place)[1:2]:
                             if get_n_spinese(cell_name)>1:
                                 os.system(send_command+" False")
 
-# command="sbatch -p ss.q,elsc.q runs_change_passive_val.sh"
-# p4=" ".join([command,'1', '2017_05_08_A_5-4', 'morphology.swc', '120.0', '2.974997902411084', '6992.0', 'const_param', 'RA=120', '1.0', '1.0', '20'])
-# p5=" ".join([command,'1', '2017_05_08_A_4-5', 'morphology.swc', '74.0', '2.072525235725758', '7485.0', 'const_param', 'RA_best_fit', '1.0', '1.0', '20'])
-# os.system(p4)
-# os.system(p5)
-#  p1='1', '2017_05_08_A_4-5', 'z_correct.swc', '120.0', '1.9249102108707408', '8059.0', 'const_param', 'RA=120', '1.1', '1.1', '20'
-#  p2='1', '2017_05_08_A_4-5', 'z_correct.swc', '150.0', '2.076686932986252', '7470.0', 'const_param', 'RA=150', '1.1', '1.1', '20'
-#  p3='1', '2017_05_08_A_4-5', 'z_correct.swc', '82.0', '1.7502935111595734', '8863.0', 'const_param', 'RA_best_fit', '1.1', '1.1', '20'
-
- # p6='1', '2017_05_08_A_4-5', 'morphology.swc', '120.0', '1.9249102108707408', '8059.0', 'const_param', 'RA=120', '1.1', '1.1', '20', '_'
- # p7='1', '2017_05_08_A_4-5', 'morphology.swc', '150.0', '2.076686932986252', '7470.0', 'const_param', 'RA=150', '1.1', '1.1', '20', '_'
- # p8='1', '2017_05_08_A_4-5', 'morphology.swc', '82.0', '1.7502935111595734', '8863.0', 'const_param', 'RA_best_fit', '1.1', '1.1', '20', '_'
- # p9='1', '2017_05_08_A_4-5', 'morphology.swc', '120.0', '1.8406325806131112', '8428.0', 'const_param', 'RA=120', '1.2', '1.0', '20', '_'
- # p10='1', '2017_05_08_A_4-5', 'morphology.swc', '150.0', '1.94934046109667', '7958.0', 'const_param', 'RA=150', '1.2', '1.0', '20', '_'
- # p11='1', '2017_05_08_A_4-5', 'morphology.swc', '107.0', '1.7956767437674848', '8639.0', 'const_param', 'RA_best_fit', '1.2', '1.0', '20', '_
