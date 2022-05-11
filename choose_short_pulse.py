649a8ff14b46ee7479fa7150bd17243debe2283f
moriafrid <moria.fridman100@gmail.com>
2022-05-11 17:35:11 +0300

    wonderful function working to choose the rigth synapses

--- choose_short_pulse.py
+++ choose_short_pulse.py
@@ -18,7 +18,8 @@ dt = 0.1
 fig, ax = plt.subplots(1, 1)
 timer = fig.canvas.new_timer(interval=3000)
 timer.add_callback(close_event)
-for cell in read_from_pickle('cells_name2.p')[5:]:#[ '2017_03_04_A_6-7(0)(0)','2017_05_08_A_5-4(0)(0)','2017_05_08_A_4-5(0)(0)']:
+check='again'
+for cell in read_from_pickle('cells_name2.p')[:7]:#[ '2017_03_04_A_6-7(0)(0)','2017_05_08_A_5-4(0)(0)','2017_05_08_A_4-5(0)(0)']:
     base_dir="cells_outputs_data_short/"+cell+"/data/electrophysio_records/short_pulse/"
     print(cell)
     data=read_from_pickle(base_dir+"/clear0_short_pulse.p")
@@ -28,39 +29,59 @@ for cell in read_from_pickle('cells_name2.p')[5:]:#[ '2017_03_04_A_6-7(0)(0)','2
 
     short_pulse_mean=np.mean(npV,axis=0)
     start_short_pulse,end_short_pulse=find_short_pulse_edges(short_pulse_mean)
-
-
+    timer = fig.canvas.new_timer(interval=10000)
+    timer.add_callback(close_event)
+    for v in npV:
+        plt.plot(v)
+    plt.title(cell+' have '+str(len(npV))+' traces')
+    plt.show()
+    timer = fig.canvas.new_timer(interval=2000)
+    timer.add_callback(close_event)
     run_again='y'
+    check='again'
+
     while run_again=="y":
-        timer = fig.canvas.new_timer(interval=4000)
-        timer.add_callback(close_event)
+
         # filterd_traces_first = data[0]
         filterd_traces_first = []
         correct_traces=[]
+
         for i,trace in enumerate(npV):
-            # if i>2 : continue
-            add_figure(cell+'\ntrace_number '+str(int(i))+ ' out of '+str(len(npV)),'dots','mV')
-            for trace1 in npV:
-                base_line = trace1[start_short_pulse+start_calculate_E_pas:start_short_pulse+end_calculate_E_pas].mean()
-                plt.plot(trace1-base_line,alpha=0.3, color="k")
-                # plt.plot(np.array(data[1]), trace1-base_line, color="k")
+            # if i >2 : continue
+            timer = fig.canvas.new_timer(interval=2000)
+            timer.add_callback(close_event)
+            check='again'
 
-            plt.plot(short_pulse_mean,color='g')
-            base_line = trace[start_short_pulse+start_calculate_E_pas:start_short_pulse+end_calculate_E_pas].mean()
-            len_E_pas=(start_short_pulse+end_calculate_E_pas)-(start_short_pulse+start_calculate_E_pas)
-            plt.plot(np.arange(start_short_pulse+start_calculate_E_pas,start_short_pulse+end_calculate_E_pas),base_line*np.ones(len_E_pas),color="r")
-            plt.plot(trace-base_line, color="r")
-            timer.start()
+            while check=='again':
 
-            plt.show()
+                add_figure(cell+'\ntrace_number '+str(int(i))+ ' out of '+str(len(npV)),'dots','mV')
+                for trace1 in npV:
+                    base_line = trace1[start_short_pulse+start_calculate_E_pas:start_short_pulse+end_calculate_E_pas].mean()
+                    plt.plot(trace1-base_line,alpha=0.3, color="k")
+                    # plt.plot(np.array(data[1]), trace1-base_line, color="k")
 
-            check = input('is the trace good bad or ugly? (good=enter,bad=b)')
-            pas = np.mean(trace[start_short_pulse+start_calculate_E_pas])
+                plt.plot(short_pulse_mean,color='g')
+                base_line = trace[start_short_pulse+start_calculate_E_pas:start_short_pulse+end_calculate_E_pas].mean()
+                len_E_pas=(start_short_pulse+end_calculate_E_pas)-(start_short_pulse+start_calculate_E_pas)
+                plt.plot(np.arange(start_short_pulse+start_calculate_E_pas,start_short_pulse+end_calculate_E_pas),base_line*np.ones(len_E_pas),color="r")
+                plt.plot(trace-base_line, color="r")
+                timer.start()
+
+                plt.show()
+
+                check = input('is the trace good bad or ugly? (good=enter,bad=b or again)')
+                pas = np.mean(trace[start_short_pulse+start_calculate_E_pas:start_short_pulse+end_calculate_E_pas])
+
             if check == '':
                 filterd_traces_first.append(trace-pas)
                 correct_traces.append(i)
-            # filterd_traces_first.append(trace)
+
+            elif check=='end':
+                run_again='no'
+                break
+            continue
         filterd_traces_first = np.array(filterd_traces_first)
+
 
         try:(np.savetxt(base_dir+"/peeling.txt", "traces number is "+str(correct_traces)+"\n"+[data[1], np.mean(filterd_traces_first,axis=0).flatten()*data[0].units]))
         except:"txt not secsseed to save"
@@ -86,8 +107,6 @@ for cell in read_from_pickle('cells_name2.p')[5:]:#[ '2017_03_04_A_6-7(0)(0)','2
         for v in filterd_traces_first:
             plt.plot(v)
         plt.show()
+
         run_again=input("run again? (y/enter)")
 
-
-
-    plt.close()
