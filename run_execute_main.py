1da282fd36aa4bb8d253994db1b4a7ec1f8326cf
moriafrid <moria.fridman100@gmail.com>
2022-05-12 19:05:28 +0300

    change to run from cluster

--- run_execute_main.py
+++ run_execute_main.py
@@ -15,10 +15,10 @@ print("Remaind to choose the right syn")
 folder_data="cells_initial_information"
 folder_save="cells_outputs_data_short"
 for cell_name in cells:
-    # command="sbatch execute_main.sh"
-    # send_command = " ".join([command, cell_name,folder_, folder_data , folder_save])
-    command= "python main_cell_data.py"
-    send_command = " ".join([command, cell_name])
+    command="sbatch execute_main.sh"
+    send_command = " ".join([command, cell_name,folder_, folder_data , folder_save])
+    # command= "python main_cell_data.py"
+    # send_command = " ".join([command, cell_name])
     print(cell_name+str(' :run main_cell_data.py'))
     os.system(send_command)
 print("Remaind to choose the right syn")
