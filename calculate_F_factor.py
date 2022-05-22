5eed78424483789284b7578efe395ce341fd2b0d
moriafrid <moria.fridman100@gmail.com>
2022-05-22 18:30:28 +0300

    change the density to be const - the results is 1.57

--- calculate_F_factor.py
+++ calculate_F_factor.py
@@ -9,12 +9,12 @@ def calculate_F_factor(cell,spine_type='mean_spine',spines_density=1.08,spine_nu
     if is_debug_print:
         print("the spine_type for calculate the F_factor is "+spine_type)
     spine_area=get_parameter(spine_type,'spine_area')[0]
-    spines_density_temp=get_parameter(spine_type,'spine_density')[0]
-    if not np.isnan(spines_density_temp):
-        if spines_density!=spines_density_temp:
-            spines_density=spines_density
-        else:
-            spines_density=spines_density_temp
+    # spines_density_temp=get_parameter(spine_type,'spine_density')[0]
+    # if not np.isnan(spines_density_temp):
+    #     if spines_density!=spines_density_temp:
+    #         spines_density=spines_density
+    #     else:
+    #         spines_density=spines_density_temp
     print("spine density="+str(spines_density))
     if spine_type!='mean_spine':
         R_head,neck_diam,neck_length=get_F_factor_params(spine_type)
@@ -40,3 +40,4 @@ if __name__=='__main__':
     cell=load_swc('cells_initial_information/2017_05_08_A_4-5/morphology_z_correct.swc')
     # cell=load_ASC('cells_initial_information/2017_05_08_A_5-4/05_08_A_01062017_Splice_shrink_FINISHED_LABEL_Pinkcell.ASC')
     F=calculate_F_factor(cell)
+    print(F)
