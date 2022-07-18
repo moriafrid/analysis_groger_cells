8c851aabf524ffc6401ce893bb5e923e53e9550c
moriafrid <moria.fridman100@gmail.com>
2022-07-18 14:47:08 +0300

    run the correction in file mean_syn.p in folder without syn_par name

--- MOO_get_parameters.py
+++ MOO_get_parameters.py
@@ -87,9 +87,9 @@ folder_=''
 data_dir= "cells_initial_information/"
 save_dir = "cells_outputs_data_short/"
 if same_strengh:
-    base2 = folder_+save_dir+cell_name+'/MOO_results_syn_par_same_strange/'+file_type+'_SPINE_START='+str(SPINE_START)+'/'  # folder name  _RA_free
+    base2 = folder_+save_dir+cell_name+'/MOO_results_same_strange/'+file_type+'_SPINE_START='+str(SPINE_START)+'/'  # folder name  _RA_free
 else:
-    base2 = folder_+save_dir+cell_name+'/MOO_results_syn_par_relative_strange/'+file_type+'_SPINE_START='+str(SPINE_START)+'/'  # folder name  _RA_free
+    base2 = folder_+save_dir+cell_name+'/MOO_results_relative_strange/'+file_type+'_SPINE_START='+str(SPINE_START)+'/'  # folder name  _RA_free
 
 base2+='F_shrinkage='+str(round(shrinkage_by,2))+'_dend*'+str(round(resize_dend_by,2))
 if double_spine_area:
