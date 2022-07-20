48ad5e8d376126f493985cceb903617d6f902a21
moriafrid <moria.fridman100@gmail.com>
2022-07-20 16:48:32 +0300

    correct the expeiment after Moo

--- run_analysis_MOO.py
+++ run_analysis_MOO.py
@@ -2,7 +2,9 @@ import os
 base="python "
 base="sbatch execute_python_script.sh "
 os.system(base+"exp_V_syns_soma.py")
+os.system(base+"exp_V_syns_effect_on_soma.py")
 os.system(base+"exp_soma_NMDA_AMPA.py")
+os.system(base+"exp_syns_NMDA_AMPA.py")
 os.system(base+"exp_sys_gmax.py")
 
 
