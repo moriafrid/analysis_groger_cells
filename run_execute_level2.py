d95fa028aa0fbdec9bc163f695ed010c6b9f39c4
moriafrid <moria.fridman100@gmail.com>
2022-07-25 19:48:18 +0300

    run without other morphology exceppt XYZ shrinkage

--- run_execute_level2.py
+++ run_execute_level2.py
@@ -18,7 +18,8 @@ os.system('python csv_for_passive_val_results.py cells_name2.p')
 i=0
     # os.system('python run_analysis_fit_after_run.py')
 for cell_name in read_from_pickle(cells_name_place):
-    if not cell_name in ['2017_07_06_C_4-3','2016_04_16_A']:continue#['2017_02_20_B','2017_07_06_C_3-4']:continue
+    if not cell_name in ['2017_03_04_A_6-7']:continue
+    # if not cell_name in ['2017_07_06_C_4-3','2016_04_16_A']:continue#['2017_02_20_B','2017_07_06_C_3-4']:continue
     passive_vals_dict= {}
     # p='cells_initiall_information/'+cell_name+'/results_passive_fits.csv'
     p='cells_outputs_data_short/'+cell_name+'/fit_short_pulse/results_passive_fits.csv'
@@ -33,11 +34,11 @@ for cell_name in read_from_pickle(cells_name_place):
                 else:
                     do_double_spine_area=['False']
                 for double_spine_area in do_double_spine_area:
-                    if resize_diam_by==1.0 and shrinkage_by==1.0 and double_spine_area=='False' and SPINE_START==20:
-                        file_types=['z_correct.swc','morphology.swc']
-                    else:
-                        file_types=['z_correct.swc']
-                    for file_type in file_types:
+                    # if resize_diam_by==1.0 and shrinkage_by==1.0 and double_spine_area=='False' and SPINE_START==20:
+                    #     file_types=['z_correct.swc','morphology.swc']
+                    # else:
+                    #     file_types=['z_correct.swc']
+                    for file_type in ['z_correct.swc']:
                         passive_vals_dict=get_passive_parameter(cell_name,double_spine_area=double_spine_area,shrinkage_resize=[shrinkage_by,resize_diam_by],fit_condition=fit_condition,spine_start=SPINE_START,file_type=file_type)
                         next_continue=False
                         for i, name in enumerate(['RA_min_error','RA_best_fit','RA=120','RA=150']):
