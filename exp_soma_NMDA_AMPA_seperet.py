7698546b285198d0713b0f178a2bdb8292243109
moriafrid <moria.fridman100@gmail.com>
2022-07-31 16:00:30 +0300

    seperate between the results

--- exp_soma_NMDA_AMPA_seperet.py
+++ exp_soma_NMDA_AMPA_seperet.py
@@ -9,6 +9,8 @@ import pickle
 import matplotlib
 from open_pickle import read_from_pickle
 from extraClasses import neuron_start_time
+from add_figure import add_figure
+
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['svg.fonttype'] = 'none'
 import sys
@@ -26,13 +28,57 @@ else:
 folder_= ''
 folder_data1=folder_+'cells_outputs_data_short/'+specipic_cell+'/MOO_results_same_strange'+before_after+'/*/F_shrinkage=*/const_param/'
 folder_data2=folder_+'cells_outputs_data_short/'+specipic_cell+'/MOO_results_relative_strange'+before_after+'/*/F_shrinkage=*/const_param/'
-save_name='/AMPA&NMDA_soma'
-for curr_i, model_place in tqdm(enumerate(glob(folder_data1+'*')+glob(folder_data2+'*'))):
-    # if '3-4' in model_place: continue
+save_name='/AMPA&NMDA_soma_seperete'
+color=['#03d7fc','#fcba03']
+def simulate_syn(sec,seg,num=None,color='black'):
+    global time_all
+    if num is None:
+        V_spine,spines,syn_objs=[],[],[]
+        for sec_t,seg_t,i in zip(sec,seg,range(get_n_spinese(cell_name))):
+            spine, syn_obj = loader.create_synapse(eval('model.'+sec_t), seg_t,reletive_strengths[i], number=i,netstim=netstim)
+            spines.append(spine)
+            syn_objs.append(syn_obj)
+    else:
+        spines, syn_objs = loader.create_synapse(eval('model.'+sec),seg,reletive_strengths[num], number=num,netstim=netstim)
+    time = h.Vector()
+    time.record(h._ref_t)
+    V_soma = h.Vector()
+    V_soma.record(model.soma[0](0.5)._ref_v)
+    h.dt = 0.1
+    h.steps_per_ms = 1.0/h.dt
+    h.run()
+    cut_from_start_time=int(neuron_start_time/0.1)
+    V_soma_All = np.array(V_soma)[cut_from_start_time:]
+    time_all = np.array(time)[cut_from_start_time:]
+    time_all-=time_all[0]
+    # take syn_obj to be 0 to see the NMDA
+    if num is None:
+        for j in range(get_n_spinese(cell_name)):
+            syn_objs[j][1][1].weight[0]=0
+    else:
+        syn_objs[1][1].weight[0]=0
+
+    h.steps_per_ms = 1.0/h.dt
+    h.run()
+    V_soma_AMPA = np.array(V_soma)[cut_from_start_time:]
+    V_NMDA = V_soma_All-V_soma_AMPA
+
+    # plt.plot(time_all, V_soma_All, color='g', lw=5,label='all',alpha=0.4)
+    if not num is None:
+        plt.plot(time_all, V_soma_AMPA, color=color, lw=2,linestyle='-', label='AMPA '+str(round(loader.get_param('weight_AMPA')*1000*reletive_strengths[num]/sum(reletive_strengths),3))+'nS '+str(round(reletive_strengths[num]*100))+"%",alpha=0.6)
+        plt.plot(time_all, V_NMDA+V_soma_All[0],lw=2, color=color, linestyle='--', label='NMDA '+str(round(loader.get_param('weight_NMDA')*1000*reletive_strengths[num]/sum(reletive_strengths),3))+'nS '+str(round(reletive_strengths[num]*100))+"%",alpha=0.6)
+    else:
+        plt.plot(time_all, V_soma_AMPA, color=color, lw=2,linestyle='-', label='AMPA '+str(round(loader.get_param('weight_AMPA')*1000,3))+'nS',alpha=0.6)
+        plt.plot(time_all, V_NMDA+V_soma_All[0],lw=2, color=color, linestyle='--', label='NMDA '+str(round(loader.get_param('weight_NMDA')*1000,3))+'nS',alpha=0.6)
+    return {'V_soma_AMPA':V_soma_AMPA,'V_soma_NMDA':V_NMDA+V_soma_All[0]}
+
+for curr_i, model_place in tqdm(enumerate(glob(folder_data2+'*')+glob(folder_data1+'*'))):
     print(model_place)
     type=model_place.split('/')[-1]
     cell_name=model_place.split('/')[1]
+    if get_n_spinese(cell_name)<2:continue
     if type=='test': continue
+    if "same" in model_place: continue
     try:loader = OPEN_RES(res_pos=model_place+'/', curr_i=curr_i)
     except Exception as e:
         print(e)
@@ -42,8 +88,10 @@ for curr_i, model_place in tqdm(enumerate(glob(folder_data1+'*')+glob(folder_dat
         psd_sizes=get_parameter(cell_name,'PSD')
         argmax=np.argmax(psd_sizes)
         reletive_strengths=psd_sizes/psd_sizes[argmax]
+        relative='relative'
     else:
         reletive_strengths=np.ones(get_n_spinese(cell_name))
+        relative='same'
     model=None
     model=loader.get_model()
     RDSM_objective_file = folder_+'cells_initial_information/'+cell_name+"/mean_syn.p"
@@ -64,57 +112,25 @@ for curr_i, model_place in tqdm(enumerate(glob(folder_data1+'*')+glob(folder_dat
     netstim.noise = 0
     h.tstop = total_duration
 
-    secs,segs=get_sec_and_seg(cell_name)
-    num=0
-    V_spine=[]
-    spines=[]
-    syn_objs=[]
-    for sec,seg in zip(secs,segs):
-        dict_spine_param=get_building_spine(cell_name,num)
-        spine, syn_obj = loader.create_synapse(eval('model.'+sec), seg,reletive_strengths[num], number=num,netstim=netstim)
-        spines.append(spine)
-        syn_objs.append(syn_obj)
-        # V_spine.append(h.Vector())
-        # V_spine[num].record(spine[1](1)._ref_v)
-        num+=1
-
-    # spine, syn_obj = loader.create_synapse(model.dend[82], 0.165, netstim=netstim)
-    time = h.Vector()
-    time.record(h._ref_t)
-    V_soma = h.Vector()
-    V_soma.record(model.soma[0](0.5)._ref_v)
-    h.dt = 0.1
-    h.steps_per_ms = 1.0/h.dt
-    h.run()
-    cut_from_start_time=int(neuron_start_time/0.1)
-    V_soma_All = np.array(V_soma)[cut_from_start_time:]
-    time_all = np.array(time)[cut_from_start_time:]
-    time_all-=time_all[0]
-    # take syn_obj to be 0 to see the NMDA
-    for j in range(num):
-        syn_objs[j][1][1].weight[0]=0
-    h.steps_per_ms = 1.0/h.dt
-    h.run()
-    V_soma_AMPA = np.array(V_soma)[cut_from_start_time:]
-    time_AMPA = np.array(time)[cut_from_start_time:]
-    V_NMDA = V_soma_All-V_soma_AMPA
-    from add_figure import add_figure
-
     passive_propert_title='Rm='+str(round(1.0/model.soma[0].g_pas,2)) +' Ra='+str(round(model.soma[0].Ra,2))+' Cm='+str(round(model.soma[0].cm,2))
-    fig=add_figure('AMPA and NMDA impact on voltage '+" ".join(model_place.split('/')[-1].split('_')[2:])+'\n'+passive_propert_title,'time[ms]','Voltage[mV]')
-    plt.plot(time_all, V_soma_All, color='g', lw=5,label='all',alpha=0.4)
-    plt.plot(time_all, V_soma_AMPA, color='b', lw=2,linestyle='--', label='AMPA',alpha=0.8)
-    # plt.plot(time_all, V_NMDA,lw=2, color='r', linestyle='--', label='NMDA',alpha=0.8)
-    plt.plot(time_all, V_NMDA+V_soma_All[0],lw=2, color='r', linestyle='--', label='NMDA',alpha=0.8)
-    plt.plot(T_base, np.array(V_base)+loader.get_param('e_pas'), color='black',label='EP record',alpha=0.2,lw=5)
+    fig=add_figure(cell_name+' AMPA and NMDA '+relative+" strength"+'\n'+model_place.split('/')[-1]+" "+passive_propert_title,'time[ms]','Voltage[mV]')
+    plt.text(0,V_base[0]+2,reletive_strengths)
+    secs,segs=get_sec_and_seg(cell_name)
+    dict_result={}
+    for num in range(get_n_spinese(cell_name)):
+        dict_result['voltage_'+str(num)]=simulate_syn(secs[num],segs[num],num=num,color=color[num])
+    dict_result['voltage_all']=simulate_syn(secs,segs,color='black')
 
+
+    plt.plot(T_base, np.array(V_base)+loader.get_param('e_pas'), color='black',label='EP record',alpha=0.2,lw=5)
+    dict_result['voltage_all']={'experiment':np.array(V_base)+loader.get_param('e_pas')}
     plt.legend()
     print("Save ", model_place+save_name+'.png')
     plt.savefig(model_place+save_name+'.png')
     plt.savefig(model_place+save_name+'.pdf')
     pickle.dump(fig, open(model_place+save_name+'.p', 'wb'))
-    pickle.dump({'time':time_all,'voltage':{'Model':V_soma_All,'V_AMPA':V_soma_AMPA,'V_NMDA': V_NMDA+V_soma_All[0],'experiment':np.array(V_base)+loader.get_param('e_pas')}}, open(model_place+save_name+'_data.p', 'wb'))
-    # plt.show()
+    pickle.dump([{'time':time_all},dict_result], open(model_place+save_name+'_data.p', 'wb'))
+    plt.show()
     plt.close()
     loader.destroy()
     # model.destroy()
