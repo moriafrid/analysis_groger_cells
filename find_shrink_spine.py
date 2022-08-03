8ae8c2709f9d157345235c0e9d8b3b9388b08b3b
moriafrid <moria.fridman100@gmail.com>
2022-08-03 10:17:44 +0300

    cells that their XYZ not make any sense

---
+++ find_shrink_spine.py
@@ -0,0 +1,28 @@
+from glob import glob
+from extra_function import load_ASC, load_swc
+from neuron import h
+from open_pickle import read_from_pickle
+file=['ASC','z_correct.swc']
+path=glob('cells_initial_information/*/*.ASC')
+path1=glob('cells_initial_information/*/*z_correct.swc')
+for cell_name in read_from_pickle('cells_name2.p')[0:]:
+    print(cell_name)
+    path=glob('cells_initial_information/'+cell_name+'/*.ASC')[0]
+    path1=glob('cells_initial_information/'+cell_name+'/*.ASC')[1]
+
+    # path1=glob('cells_initial_information/'+cell_name+'/*z_correct.swc')[0]
+    if '(0)' in path: continue
+    print(cell_name)
+    cell=None
+    cell=load_ASC(path)
+    h.PlotShape()
+    print('len ASC dend',len(cell.dend))
+    print(sum([sec.L for sec in cell.dend]))
+    a=1
+
+    cell=None
+    cell=load_swc(path1)
+    print('len z_correct.swc dend',len(cell.dend))
+    print(sum([sec.L for sec in cell.dend]))
+
+    a=1
