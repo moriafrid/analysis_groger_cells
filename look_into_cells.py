bfd5c07ba3e49cb9aead149e16a89c0ef1d2047b
moriafrid <moria.fridman100@gmail.com>
2022-08-03 10:34:47 +0300

    if there is more then 2 ASC files

--- look_into_cells.py
+++ look_into_cells.py
@@ -2,24 +2,17 @@ from glob import glob
 from extra_function import load_ASC, load_swc
 from neuron import h
 from open_pickle import read_from_pickle
-file=['ASC','z_correct.swc']
-path=glob('cells_initial_information/*/*.ASC')
-path1=glob('cells_initial_information/*/*z_correct.swc')
-for cell_name in read_from_pickle('cells_name2.p')[-1:]:
+from read_spine_properties import get_sec_and_seg
+for cell_name in read_from_pickle('cells_name2.p')[0:]:
     path=glob('cells_initial_information/'+cell_name+'/*.ASC')[0]
-    path1=glob('cells_initial_information/'+cell_name+'/*z_correct.swc')[0]
-    if '(0)' in path: continue
+    path1=glob('cells_initial_information/'+cell_name+'/*.ASC')[1]
+    print(cell_name)
+
+    cell=load_ASC(path)
+
     print(cell_name)
     cell=None
     cell=load_ASC(path)
     h.PlotShape()
     print('len ASC dend',len(cell.dend))
     print(sum([sec.L for sec in cell.dend]))
-    a=1
-
-    cell=None
-    cell=load_swc(path1)
-    print('len z_correct.swc dend',len(cell.dend))
-    print(sum([sec.L for sec in cell.dend]))
-
-    a=1
