58c67b579201982e70a072fd78f42e4b1dec9c12
moriafrid <moria.fridman100@gmail.com>
2022-08-08 12:11:30 +0300

    not imprtant

--- run_all.py
+++ run_all.py
@@ -1,16 +1,5 @@
 import os
 from open_pickle import read_from_pickle
-#set invisible top and right, for the 1st column (can remove the if)
-# for ax in fig.get_axes():  #show only specific spines
-#     if not ax.get_subplotspec().is_first_col():
-#         ax.spines['top'].set_visible(False)
-#         ax.spines['right'].set_visible(False)
-#
-#
-# matplotlib.rcParams['pdf.fonttype'] = 42
-# matplotlib.rcParams['svg.fonttype'] = 'none'
-#fontsize as parameter for xlabel etc (for everything, title...)
-
 cells_name_place='cells_name2.p'
 
 
@@ -21,7 +10,7 @@ for cell_name in read_from_pickle(cells_name_place):
     os.system('python file_converter_to_swc_z_corrections.py '+cell_name)
     # os.system('python creat_morphology_dict.py '+cell_name)
     os.systen('python plot_morphology_David.py '+cell_name)
-
+os.system('python file_converter_to_swc_z_corrections.py')
 
 # commet_level1=" ".join(["python plot_morphology_David.py", cells_name_place ])
 # os.system(commet_level1)
@@ -37,7 +26,7 @@ os.system(commet_level1)
 
 os.syatem("csv_for_passive_val_results.py")
 
-commet_level1=" ".join(["python run_execute_level2.py", cells_name_place ])
+commet_level1=" ".join(["python run_execute_level2.py", cells_name_place  ])
 os.system(commet_level1)
 
 os.system('python choose_syn.py')
