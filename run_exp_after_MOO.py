24701d432c406552e4495f201859df883d8f338c
moriafrid <moria.fridman100@gmail.com>
2022-08-17 14:00:27 +0300

    switch between the script

--- run_exp_after_MOO.py
+++ run_exp_after_MOO.py
@@ -1,6 +1,14 @@
 import os
-os.system("sbatch execute_python_script.sh exp_V_syns_soma.py")
-os.system("sbatch execute_python_script.sh exp_soma_NMDA_AMPA.py")
-os.system("sbatch execute_python_script.sh exp_sys_gmax.py")
-
+before_after='_after_shrink'
+base="python "
+base="sbatch execute_python_script.sh "
+cell2run='None'
+specific_moo=' _correct_seg'
+os.system("python csv_for_MOO_results.py "+ cell2run+' '+before_after+specific_moo)
+os.system(base+"exp_soma_NMDA_AMPA_seperet.py "+ cell2run+' '+ before_after+specific_moo)
+os.system(base+"exp_V_syns_effect_on_soma.py "+ cell2run+' '+before_after+specific_moo)
+os.system(base+"exp_soma_NMDA_AMPA.py "+ cell2run+' '+before_after+specific_moo)
+# os.system(base+"exp_syns_NMDA_AMPA.py "+before_after)
+os.system(base+"exp_sys_gmax.py "+ cell2run+' '+before_after+specific_moo)
+os.system(base+"exp_V_in_Rneck.py "+ cell2run+' '+before_after+specific_moo)
 
