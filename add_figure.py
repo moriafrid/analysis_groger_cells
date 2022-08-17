c5db4074f9ef330471d396f1baaf6350b6134faf
moriafrid <moria.fridman100@gmail.com>
2022-08-17 09:51:39 +0300

    modifue the text definition

--- add_figure.py
+++ add_figure.py
@@ -5,6 +5,14 @@ matplotlib.rcParams['svg.fonttype'] = 'none'
 def add_figure(title,x_label,y_label):
 	plt.close()
 	figure=plt.figure()
+	fontsize=16
+	plt.rc('font', size=20)          # controls default text sizes
+	plt.rc('axes', titlesize=16)     # fontsize of the axes title
+	plt.rc('axes', labelsize=16)    # fontsize of the x and y labels
+	plt.rc('xtick', labelsize=10)    # fontsize of the tick labels
+	plt.rc('ytick', labelsize=10)    # fontsize of the tick labels
+	plt.rc('legend', fontsize=16)    # legend fontsize
+	plt.rc('figure', titlesize=22)  # fontsize of the figure title
 	plt.title(title,fontsize=15)
 	plt.xlabel(x_label)
 	plt.ylabel(y_label)
