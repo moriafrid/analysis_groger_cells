a9b426d0df0a9092e0c84e7b94cbce764278d9d8
moriafrid <moria.fridman100@gmail.com>
2022-08-21 10:01:31 +0300

    add figure for subplot

--- add_figure.py
+++ add_figure.py
@@ -5,7 +5,6 @@ matplotlib.rcParams['svg.fonttype'] = 'none'
 def add_figure(title,x_label,y_label):
 	plt.close()
 	figure=plt.figure()
-	fontsize=16
 	plt.rc('font', size=20)          # controls default text sizes
 	plt.rc('axes', titlesize=16)     # fontsize of the axes title
 	plt.rc('axes', labelsize=16)    # fontsize of the x and y labels
@@ -17,3 +16,16 @@ def add_figure(title,x_label,y_label):
 	plt.xlabel(x_label)
 	plt.ylabel(y_label)
 	return figure
+
+
+def adgust_subplot(ax,title,x_label,y_label):
+	plt.rc('font', size=16)          # controls default text sizes
+	plt.rc('axes', titlesize=16)     # fontsize of the axes title
+	plt.rc('axes', labelsize=16)    # fontsize of the x and y labels
+	plt.rc('xtick', labelsize=10)    # fontsize of the tick labels
+	plt.rc('ytick', labelsize=10)    # fontsize of the tick labels
+	plt.rc('legend', fontsize=16)    # legend fontsize
+	plt.rc('figure', titlesize=30)  # fontsize of the figure title
+	ax.set_title(title,fontsize=15)
+	ax.set_xlabel(x_label)
+	ax.set_ylabel(y_label)
