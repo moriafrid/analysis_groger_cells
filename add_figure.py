a0d5fe21476657a4596f65c5841e90d877df1a8b
moriafrid <moria.fridman100@gmail.com>
2022-08-24 18:52:27 +0300

    change the size of the plt.rc

--- add_figure.py
+++ add_figure.py
@@ -19,13 +19,13 @@ def add_figure(title,x_label,y_label):
 
 
 def adgust_subplot(ax,title,x_label,y_label):
-	plt.rc('font', size=16)          # controls default text sizes
-	plt.rc('axes', titlesize=16)     # fontsize of the axes title
-	plt.rc('axes', labelsize=16)    # fontsize of the x and y labels
-	plt.rc('xtick', labelsize=10)    # fontsize of the tick labels
-	plt.rc('ytick', labelsize=10)    # fontsize of the tick labels
-	plt.rc('legend', fontsize=16)    # legend fontsize
-	plt.rc('figure', titlesize=30)  # fontsize of the figure title
-	ax.set_title(title,fontsize=15)
+	plt.rc('font', size=20)          # controls default text sizes
+	plt.rc('axes', titlesize=24)     # fontsize of the axes title
+	plt.rc('axes', labelsize=24)    # fontsize of the x and y labels
+	plt.rc('xtick', labelsize=18)    # fontsize of the tick labels
+	plt.rc('ytick', labelsize=18)    # fontsize of the tick labels
+	#plt.rc('legend', fontsize=30)    # legend fontsize
+	plt.rc('figure', titlesize=40)  # fontsize of the figure title
+	ax.set_title(title,fontsize=30)
 	ax.set_xlabel(x_label)
 	ax.set_ylabel(y_label)
