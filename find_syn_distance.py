2852749f828af8f311d6173df09c48b562455ff5
moriafrid <moria.fridman100@gmail.com>
2022-08-29 12:18:57 +0300

    easy to run from cluster

--- find_syn_distance.py
+++ find_syn_distance.py
@@ -1,12 +1,18 @@
 from calculate_F_factor import calculate_F_factor
-from create_Figures import find_RA
+from function_Figures import find_RA
 from extra_function import load_swc
 from open_pickle import *
 from read_spine_properties import get_sec_and_seg, get_parameter
 from neuron import h
 from glob import glob
 import numpy as np
-cell_name=read_from_pickle('cells_name2.p')[9] #@need to be run forn the consule and the cell number need to be change
+import sys
+if len(sys.argv[1])!=2:
+    numver=0
+else:
+    numbe=int(sys.argv[1])
+cell_name=read_from_pickle('cells_name2.p')[numbe]#@need to be run form the consule and the cell number need to be change
+print(cell_name)
 cell_file=glob('cells_initial_information/'+cell_name+'/*after_shrink.swc')[0]
 cell=load_swc(cell_file)
 SPINE_START=20
@@ -60,7 +66,7 @@ def change_model_pas(cell,CM=1, RA = 250, RM = 20000.0, E_PAS = -70.0,F_factor=1
 
 base_dir='final_data/'+cell_name+'/'
 decided_passive_params=find_RA(base_dir)
-dict_result=read_from_pickle(glob(base_dir+decided_passive_params+'_results.p')[0])['parameter']
+dict_result=read_from_pickle(glob(base_dir+decided_passive_params+'_pickles.p')[0])['parameter']
 cell=change_model_pas(cell,CM=dict_result['CM'], RA = dict_result['RA'], RM = dict_result['RM'], E_PAS = dict_result['E_PAS'],F_factor=calculate_F_factor(cell))
 i=0
 for sec,seg in zip(get_sec_and_seg(cell_name)[0],get_sec_and_seg(cell_name)[1]):
