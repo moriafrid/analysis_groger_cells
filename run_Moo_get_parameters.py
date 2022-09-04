47fcd1ef3e0f4095c8a7295369210ac5c25aa855
moriafrid <moria.fridman100@gmail.com>
2022-09-04 13:07:28 +0300

    run again cell 4_03_B - now I allmost sure it's on the correct cell

--- run_Moo_get_parameters.py
+++ run_Moo_get_parameters.py
@@ -25,8 +25,8 @@ os.system('python csv_for_passive_val_results.py cells_name2.p '+before_after)
 
 
 for cell_name in read_from_pickle(cells_name_place):
-    # if cell_name!='2017_04_03_B':continue
-    if not cell_name  in ['2016_05_12_A','2016_08_30_A']:continue
+    if cell_name!='2017_04_03_B':continue
+    # if not cell_name  in ['2016_05_12_A','2016_08_30_A']:continue
     passive_vals_dict= {}
     p='cells_outputs_data_short/'+cell_name+'/fit_short_pulse'+before_after+'/results_passive_fits.csv'
     print(cell_name)
