7e7a674c4e1b4231ce9aa905f0244b2ded6a4f2c
moriafrid <moria.fridman100@gmail.com>
2022-09-04 10:57:54 +0300

    the orgenize results is done

--- csv_for_MOO_results_final.py
+++ csv_for_MOO_results_final.py
@@ -8,6 +8,9 @@ from passive_val_function import *
 import sys
 from read_passive_parameters_csv import get_passive_parameter
 import re
+
+from read_spine_properties import get_n_spinese
+
 if len(sys.argv) != 3:
     cells_name_place="cells_name2.p"
     before_after='_after_shrink'
@@ -16,7 +19,7 @@ else:
     cells_name_place=sys.argv[1]
     before_after=sys.argv[2]
     print("creat csv for passive_val running with sys.argv",sys.argv)
-save_moo=''
+save_moo='_Rin_result'
 folder_=""
 folder_data="cells_initial_information/"
 folder_save="cells_outputs_data_short/"
@@ -25,66 +28,73 @@ data_file='cells_outputs_data_short/'
 
 
 short_pulse='/fit_short_pulse'+before_after+'/'
-file_type2read=['z_correct.swc','morphology.swc','hoc','ASC']
 i=0
 # os.system('python run_analysis_fit_after_run.py')
+
 for cell_name in cells:
     print(cell_name)
+    all_data = []
+    file=[]
+    for p in MOO_file(cell_name,before_after=before_after):
+        for passive_param_name in ['RA_min_error','RA_best_fit','RA=100','RA=120','RA=150','RA=200','RA=300']:
+            file+=glob(folder_save+cell_name+p+'/F_shrinkage=1.0*1.0/const_param/'+passive_param_name+'/Rins_pickles.p')
+    moo_total_dict={}
+    for loc in tqdm(file):
+        moo_total_dict={}
+        print(loc)
+        # df = pd.read_csv('cells_initial_information/'+cell_name+'/results_passive_fits.csv')
+        passive_param_name=loc.split('/')[6]
+        if 'test' in passive_param_name: continue
+        # file_type=loc.split('/')[3][:loc.split('/')[3].rfind('.')+4]
+        shrinkage_resize=re.findall(r"[-+]?\d*\.\d+|\d+", loc.split('/')[4])
+        shrinkage_resize=[float(num) for num in shrinkage_resize]
+        if 'double_spine_area' in loc.split('/')[4]:
+            print('double_spine_area')
+            double_spine_area='True'
+        else:
+            double_spine_area='False'
+        # spine_start=int(re.findall(r"[-+]?\d*\.\d+|\d+", loc.split('/')[3])[0])
+        if "full_trace" in passive_param_name:
+            passive_param_name=passive_param_name[:passive_param_name.rfind('_full_trace')]
 
-    for before_after in ['_before_shrink','_after_shrink']:
-        all_data = []
-        file=[]
-        for p in MOO_file(cell_name,before_after=before_after):
-            file+=glob(folder_save+cell_name+p+'/F_shrinkage=*/const_param/*/hall_of_fame.p')
+        result_dict=read_from_pickle(loc)
 
-        # file+=glob(folder_save+cell_name+MOO_relative+'/F_shrinkage=*/const_param/*/hall_of_fame.p')
+        for i in range(get_n_spinese(cell_name)):
+            Moo_dict = {}
+            for value in ['PSD','distance']:
+                Moo_dict[value]=result_dict['parameters'][value][i]
+            relative_PSD=result_dict['parameters']['PSD']/max(result_dict['parameters']['PSD'])
+            for value in ['W_AMPA','W_NMDA']:
+                Moo_dict[value]=result_dict['parameters'][value]*1000*relative_PSD[i]/sum(relative_PSD)
 
-        for loc in tqdm(file):
-            # df = pd.read_csv('cells_initial_information/'+cell_name+'/results_passive_fits.csv')
-            passive_param_name=loc.split('/')[6]
-            if 'test' in passive_param_name: continue
-            file_type=loc.split('/')[3][:loc.split('/')[3].rfind('.')+4]
-            shrinkage_resize=re.findall(r"[-+]?\d*\.\d+|\d+", loc.split('/')[4])
-            shrinkage_resize=[float(num) for num in shrinkage_resize]
-            if 'double_spine_area' in loc.split('/')[4]:
-                print('double_spine_area')
-                double_spine_area='True'
-            else:
-                double_spine_area='False'
-            spine_start=int(re.findall(r"[-+]?\d*\.\d+|\d+", loc.split('/')[3])[0])
-            if "full_trace" in passive_param_name:
-                passive_param_name=passive_param_name[:passive_param_name.rfind('_full_trace')]
-            #this function should return all the
-            passive_vals_dict=get_passive_parameter(cell_name,before_after,passive_param_name=passive_param_name,double_spine_area=double_spine_area,
-                                                    shrinkage_resize=shrinkage_resize,spine_start=spine_start,fit_condition=loc.split('/')[5],
-                                                    file_type=file_type)
-            results_Moo=read_from_pickle(loc)
-            params=results_Moo['parameters']
-            values=np.mean(results_Moo['hall_of_fame'],axis=0)
-            Moo_dict={}
-            for par,val in zip(params,values):
-                Moo_dict[par]=val
-            i=0
-            value=passive_vals_dict
-            dict_for_records = {}
+            for value in ['tau1_AMPA','tau2_AMPA','tau1_NMDA','tau2_NMDA','E_PAS','RA','RM','CM']:
+                Moo_dict[value]=result_dict['parameters'][value]
+            Moo_dict['soma_Rin']=result_dict['soma']['Rin']
 
-            if value is not None:
-                dict_for_records.update(value)
-            # dict_for_records['parameters_type']=key
-            dict_for_records['double_spine_area']=double_spine_area
-            if "same" in loc.split('/')[2]:
-                dict_for_records['same_strange']=True
-            elif "relative" in loc.split('/')[2]:
-                dict_for_records['same_strange']=False
-            if Moo_dict is not None:
-                dict_for_records.update(Moo_dict)
-            all_data.append(dict_for_records)
-            output_df = pd.DataFrame.from_records(all_data)
-            dict_for_records['before_shrink']=before_after.split('_')[1]
-            'cells_outputs_data_short/2017_07_06_C_4-3/MOO_results_same_strange_after_shrink_correct_seg_syn_from_picture/z_correct.swc_SPINE_START=20/F_shrinkage=1.0_dend*1.0/const_param/RA_best_fit_full_trace/Rins_pickles.p'
-            # Rin_dict=read_from_pickle(glob(loc[:loc.find('hall_of_fame')]+'Rins_pickles.p')[0])
-            # dict_for_records.update(Rin_dict)
-            # all_data.append(dict_for_records)
+            for value in ['neck_base','spine_head']:
+                for value1 in ['Rin','Rtrans']:
+                    Moo_dict[value+'_'+value1]=result_dict[value][value1][i]
+            moo_total_dict[passive_param_name]=Moo_dict
+            for key, value in moo_total_dict.items():
+                dict_for_records = {}
+                # add metadata
+                dict_for_records['cell_name']=cell_name
+                dict_for_records['passive_parameter']=passive_param_name
+                dict_for_records['RA']=result_dict['parameters']['RA']
+                dict_for_records['relative']=result_dict['parameters']['reletive_strengths']
+                dict_for_records['shrinkage&resize_factors']=shrinkage_resize
+                dict_for_records['double_spine_area']=double_spine_area
+                dict_for_records['before_shrink']=before_after.split('_')[1]
+                dict_for_records['syn_num']=i
+                if value is not None:
+                    dict_for_records.update(value)
+                all_data.append(dict_for_records)
+
+        # all_data.append(dict_for_records)
+        output_df = pd.DataFrame.from_records(all_data)
+        # Rin_dict=read_from_pickle(glob(loc[:loc.find('hall_of_fame')]+'Rins_pickles.p')[0])
+        # dict_for_records.update(Rin_dict)
+        # all_data.append(dict_for_records)
 
 
 
