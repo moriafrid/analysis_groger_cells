7d993f00d3076fd4b3033e7c458209200d6849fe
moriafrid <moria.fridman100@gmail.com>
2022-09-06 22:21:31 +0300

    run os. for short-pulse model

--- reorgenize_results.py
+++ reorgenize_results.py
@@ -1,3 +1,5 @@
+import os
+
 from find_MOO_file import MOO_file
 import re
 from matplotlib import pyplot as plt
@@ -44,14 +46,15 @@ def copy_file(copy,paste,extra_name=''):
         except:
             print('')
 #need to run again with 3,5,8 if os.system is runing
+MOO_same,MOO_relative=MOO_file(before_after=before_after)
+
 for cell_name in read_from_pickle('cells_name2.p'):
     if cell_name in read_from_pickle('cells_sec_from_picture.p'):
         specipic_moo='_correct_seg_syn_from_picture'
     else:
         specipic_moo='_correct_seg_find_syn_xyz'
     # continue
-    # if cell_name!='2017_03_04_A_6-7':continue
-    # if not cell_name in ['2017_07_06_C_4-3','2017_07_06_C_3-4','2017_03_04_A_6-7']:continue
+
     if cell_name=='2017_07_06_C_3-4':
         full=''
     else:
@@ -59,7 +62,6 @@ for cell_name in read_from_pickle('cells_name2.p'):
 
     save_file='final_data/'+specipic_moo_save+'/'+cell_name+'/'
     data_file='cells_outputs_data_short/'+cell_name+'/'
-    MOO_same,MOO_relative=MOO_file()
     short_pulse='/fit_short_pulse'+before_after+'/'
     print(cell_name)
 
@@ -100,7 +102,7 @@ for cell_name in read_from_pickle('cells_name2.p'):
         if RA<50:
             continue
         else:
-            if RA>70:
+            if RA>100:
                 next_continue=True
         dirr_passive_result=glob(data_file+short_pulse+'z_correct.swc_SPINE_START=20/dend*'+str(resize_diam_by)+'&F_shrinkage='+str(shrinkage_by)+'/const_param/RA/')[0]
         # os.system(" ".join(["python plot_fit_short_pulse.py",cell_name,str(RM), str(RA), str(CM),str(resize_diam_by),str(shrinkage_by),str(passive_val_name),before_after]))
@@ -116,22 +118,50 @@ for cell_name in read_from_pickle('cells_name2.p'):
         # copy_file(glob(data_file+'/data/cell_properties/z_correct.swc/SPINE_START=20/dend*'+str(resize_diam_by)+'&F_shrinkage='+str(shrinkage_by)+'/'+passive_val_name+'/E_dendogram/dend_only_with_syn.p')[0],save_file_resize)
         # copy_file(glob(data_file+'/data/cell_properties/z_correct.swc/SPINE_START=20/dend*'+str(resize_diam_by)+'&F_shrinkage='+str(shrinkage_by)+'/'+passive_val_name+'/M_dendogram/dend_only.p')[0],save_file_resize)
 
+
         try:
             if get_n_spinese(cell_name)>1:
                 for MOO_file in glob(data_file+MOO_relative+resize+'/const_param/'+passive_val_name+full+'/*.p'):
                     if 'before' in MOO_file.split('/')[-1]:continue
                     # print(MOO_file)
                     copy_file(MOO_file,save_file_resize,extra_name=full2+'relative_'+passive_val_name)
-                copy_file(glob(data_file+MOO_relative+resize+'/const_param/'+passive_val_name+full+'/data.txt')[0],save_file_resize,extra_name=full2+'relative_'+passive_val_name)
 
+                copy_file(glob(data_file+MOO_relative+resize+'/const_param/'+passive_val_name+full+'/data.txt')[0],save_file_resize,extra_name=full2+'relative_'+passive_val_name)
             else:
                 for MOO_file in glob(data_file+MOO_same+resize+'/const_param/'+passive_val_name+full+'/*.p'):
                     if 'before' in MOO_file.split('/')[-1]:continue
                     copy_file(MOO_file,save_file_resize,extra_name=full2+passive_val_name)
-                copy_file(glob(data_file+MOO_same+resize+'/const_param/'+passive_val_name+full+'/data.txt')[0],save_file_resize,extra_name=+full2+passive_val_name)
-
+                copy_file(glob(data_file+MOO_same+resize+'/const_param/'+passive_val_name+full+'/data.txt')[0],save_file_resize,extra_name=full2+passive_val_name)
         except: "the moo isn't finish"
 
+    for passive_val_name in ['RA_min_error','RA_best_fit','RA=100','RA=120','RA=150','RA=200','RA=300']:
+        save_file_RA=save_file+passive_val_name+'/'
+        create_folder_dirr(save_file_RA)
+        passive_vals_dict=get_passive_parameter(cell_name,before_after,double_spine_area='False',shrinkage_resize=[shrinkage_by,resize_diam_by],fit_condition='const_param',spine_start=20,file_type='z_correct.swc',passive_param_name=passive_val_name)
+        if len(full)>0:
+            full2='full_'
+        else:
+            full2=''
+        RA,CM,RM=get_passive_val(passive_vals_dict,what_return='full_result')
+        RA=str(int(float(RA)))
+        copy_file(glob(data_file+short_pulse+'z_correct.swc_SPINE_START=20/dend*'+str(resize_diam_by)+'&F_shrinkage='+str(shrinkage_by)+'/const_param/RA/fit RA='+RA+'.p')[0],save_file_RA,extra_name=passive_val_name)
+        os.system(" ".join(["python plot_fit_short_pulse.py",cell_name,str(RM), str(RA), str(CM),str(resize_diam_by),str(shrinkage_by),str(passive_val_name),before_after]))
+        copy_file(dirr_passive_result+passive_val_name+'_pickles.p',save_file_RA)
+
+        try:
+            if get_n_spinese(cell_name)>1:
+                for MOO_file in glob(data_file+MOO_relative+resize+'/const_param/'+passive_val_name+full+'/*.p'):
+                    if 'before' in MOO_file.split('/')[-1]:continue
+                    copy_file(MOO_file,save_file_RA,extra_name=full2+'relative_'+passive_val_name)
+
+                copy_file(glob(data_file+save_file_RA+resize+'/const_param/'+passive_val_name+full+'/data.txt')[0],save_file_resize,extra_name=full2+'relative_'+passive_val_name)
+
+            else:
+                for MOO_file in glob(data_file+MOO_same+resize+'/const_param/'+passive_val_name+full+'/*.p'):
+                    if 'before' in MOO_file.split('/')[-1]:continue
+                    copy_file(MOO_file,save_file_RA,extra_name=full2+passive_val_name)
+                copy_file(glob(data_file+save_file_RA+resize+'/const_param/'+passive_val_name+full+'/data.txt')[0],save_file_resize,extra_name=+full2+passive_val_name)
+        except: "the moo isn't finish"
 #diffrent in
 cell_name='2017_05_08_A_4-5'
 if cell_name in read_from_pickle('cells_sec_from_picture.p'):
