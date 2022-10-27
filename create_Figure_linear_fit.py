1fd526838e931c7d6cf663a71c55015a7d35dd73
moriafrid <moria.fridman100@gmail.com>
2022-10-27 16:06:36 +0300

    start to edit r2 score add curve_fit

--- create_Figure_linear_fit.py
+++ create_Figure_linear_fit.py
@@ -29,7 +29,10 @@ def add_curve_fit(ax,x,y,m_name='m',units=1,name_units='',x_m=0.3,start_in_zero=
     if start_in_zero:
         linear_fit=linear_fit0
     else:
-        linear_fit=linear_fit1
+        slope, intercept, r, p, se = linregress(x, y)
+        r_score=r**2
+        popt=[intercept,slope]
+        # linear_fit=linear_fit1
     # ax0_1.errorbar(all_PSD, all_AMPA, all_std_AMPA, linestyle='None')
     x_data=np.arange(0,max(x)+0.01,0.0005)
     popt, pcov = curve_fit(linear_fit, x, y)
@@ -37,6 +40,9 @@ def add_curve_fit(ax,x,y,m_name='m',units=1,name_units='',x_m=0.3,start_in_zero=
     if plot_curve:
         ax.plot(x_data, linear_fit(x_data, *popt), '-')
         ax.text(x_m,0.03,m_name+'='+str(round(popt[0]*units,2))+name_units,transform=ax.transAxes,size='16')
+    else:
+        ax.text(0.05,0.94,'p='+str(round(p,2)),transform=ax.transAxes,size='16',fontweight='bold')
+
     y_pred = linear_fit(x, *popt)
     r_score=r2_score(y, y_pred)
     ax.text(0.05,0.94,'r='+str(round(r_score,2)),transform=ax.transAxes,size='16',fontweight='bold')
@@ -95,6 +101,17 @@ for i,cell_name in enumerate(read_from_pickle('cells_name2.p')):
     all_Rtrans_spine_head=np.append(all_Rtrans_spine_head,Rtrans_spine_head)
     all_Rtrans_spine_base=np.append(all_Rtrans_spine_base, Rtrans_spine_base)
     all_Rneck=np.append(all_Rneck,Rneck)
+all_V_spine_base_NMDA,all_PSD_NMDA,all_V_spine_head_NMDA,all_V_NMDA_NMDA,color_NMDA,all_W_AMPA_NMDA,all_W_NMDA_NMDA,all_V_soma_NMDA_NMDA=[],[],[],[],[],[],[],[]
+for i,val in enumerate(all_V_NMDA):
+    if val != None :
+        all_V_spine_head_NMDA=np.append(all_V_spine_head_NMDA,all_V_spine_head[i])
+        all_V_spine_base_NMDA=np.append(all_V_spine_base_NMDA,all_V_spine_base[i])
+        all_V_NMDA_NMDA=np.append(all_V_NMDA_NMDA,val)
+        all_V_soma_NMDA_NMDA=np.append(all_V_soma_NMDA_NMDA,all_V_soma_NMDA[i])
+        color_NMDA=np.append(color_NMDA,all_color[i])
+        all_W_AMPA_NMDA=np.append(all_W_AMPA_NMDA,all_W_AMPA[i])
+        all_W_NMDA_NMDA=np.append(all_W_NMDA_NMDA,all_W_NMDA[i])
+        all_PSD_NMDA=np.append(all_PSD_NMDA,all_PSD[i])
 all_color=list(all_color)
 fig2 = plt.figure(figsize=(15, 10))  # , sharex="row", sharey="row"
 fig2.subplots_adjust(left=0.1,right=0.95,top=0.9,bottom=0.1,hspace=0.2, wspace=0.25)
@@ -144,16 +161,7 @@ adgust_subplot(ax3,'','','',latter='C')
 adgust_subplot(ax4,'','AMPA [nS]','',latter='D')
 adgust_subplot(ax5,'','','',latter='E')
 adgust_subplot(ax6,'','NMDA [nS]','',latter='F')
-all_V_spine_base_NMDA,all_V_spine_head_NMDA,all_V_NMDA_NMDA,color_NMDA,all_W_AMPA_NMDA,all_W_NMDA_NMDA,all_V_soma_NMDA_NMDA=[],[],[],[],[],[],[]
-for i,val in enumerate(all_V_NMDA):
-    if val != None :
-        all_V_spine_head_NMDA=np.append(all_V_spine_head_NMDA,all_V_spine_head[i])
-        all_V_spine_base_NMDA=np.append(all_V_spine_base_NMDA,all_V_spine_base[i])
-        all_V_NMDA_NMDA=np.append(all_V_NMDA_NMDA,val)
-        all_V_soma_NMDA_NMDA=np.append(all_V_soma_NMDA_NMDA,all_V_soma_NMDA[i])
-        color_NMDA=np.append(color_NMDA,all_color[i])
-        all_W_AMPA_NMDA=np.append(all_W_AMPA_NMDA,all_W_AMPA[i])
-        all_W_NMDA_NMDA=np.append(all_W_NMDA_NMDA,all_W_NMDA[i])
+
 
 dict4plot={'ax1':[all_PSD,all_V_spine_head],'ax2':[all_PSD,all_V_spine_base],'ax3':[all_W_AMPA,all_V_spine_head],
  'ax4':[all_W_AMPA,all_V_spine_base],'ax5':[all_V_NMDA_NMDA,all_V_spine_head_NMDA],'ax6':[all_V_NMDA_NMDA,all_V_spine_base_NMDA]}
@@ -172,7 +180,7 @@ plt.savefig(save_dir+'/V_high.png')
 plt.savefig(save_dir+'/V_high.svg')
 # plt.show()
 fig1 = plt.figure(figsize=(15, 6))  # , sharex="row", sharey="row"
-fig1.subplots_adjust(left=0.1,right=0.95,top=0.85,bottom=0.15,hspace=0.01, wspace=0.2)
+fig1.subplots_adjust(left=0.1,right=0.95,top=0.85,bottom=0.15,hspace=0.01, wspace=0.3)
 shapes = (1, 3)
 plt.title('gmax AMPA against NMDA')
 ax1 = plt.subplot2grid(shape=shapes, loc=(0, 0), rowspan=1, colspan=1)
@@ -194,14 +202,14 @@ plt.savefig(save_dir+'/NMDA against AMPA.png')
 plt.savefig(save_dir+'/NMDA against AMPA.svg')
 
 fig2 = plt.figure(figsize=(6, 6))  # , sharex="row", sharey="row"
-fig2.subplots_adjust(left=0.25,right=0.95,top=0.95,bottom=0.15,hspace=0.2, wspace=0.25)
+fig2.subplots_adjust(left=0.2,right=0.95,top=0.95,bottom=0.15,hspace=0.2, wspace=0.25)
 shapes = (1, 1)
 plt.title('Synaptic current against PSD')
 ax1 = plt.subplot2grid(shape=shapes, loc=(0, 0), rowspan=1, colspan=1)
 # ax2 = plt.subplot2grid(shape=shapes, loc=(0, 1), colspan=1, rowspan=1)
 adgust_subplot(ax1,'','PSD [um^2]','Isyn [nA]',latter='',xylabelsize=1,xytitlesize=100)
 # adgust_subplot(ax2,'','PSD [um^2]','I spine base [nA]',latter='B')
-plot_dict={'label':cell_name,'lw':scatter_size-1}
+plot_dict={'label':cell_name,'lw':scatter_size-1,'color':all_color}
 ax1.scatter(all_PSD,all_I_spine_head,color=all_color,**plot_dict)
 #ax2.scatter(all_PSD,I_spine_base,**plot_dict)
 add_curve_fit(ax1,all_PSD,all_I_spine_head,name_units='um^2/pA')
@@ -213,56 +221,25 @@ fig1 = plt.figure(figsize=(15, 6))  # , sharex="row", sharey="row"
 fig1.subplots_adjust(left=0.1,right=0.95,top=0.85,bottom=0.15,hspace=0.01, wspace=0.2)
 shapes = (1, 3)
 plt.title('RA_min_error')
-ax0_1 = plt.subplot2grid(shape=shapes, loc=(0, 0), rowspan=1, colspan=1)
-ax0_2 = plt.subplot2grid(shape=shapes, loc=(0, 1), colspan=1, rowspan=1)
-ax0_3 = plt.subplot2grid(shape=shapes, loc=(0, 2), colspan=1, rowspan=1)
+ax1 = plt.subplot2grid(shape=shapes, loc=(0, 0), rowspan=1, colspan=1)
+ax2 = plt.subplot2grid(shape=shapes, loc=(0, 1), colspan=1, rowspan=1)
+ax3 = plt.subplot2grid(shape=shapes, loc=(0, 2), colspan=1, rowspan=1)
 
-adgust_subplot(ax0_1,'AMPA-PSD' ,'PSD [um^2]','g_max AMPA [nS]',latter='A')
-adgust_subplot(ax0_2,'NMDA-PSD','PSD [um^2]','g NMDA [nS]',latter='B')
-adgust_subplot(ax0_3,'AMPA-NMDA','gmax AMPA [nS]','Vmax NMDA [mV]',latter='C')
-all_AMPA,all_std_AMPA,all_NMDA,all_std_NMDA,all_PSD,all_AMPA_NMDA,all_PSD_NMDA,all_W_NMDA,all_V_NMDA,all_color=[],[],[],[],[],[],[],[],[],[]
-for i,cell_name in enumerate(read_from_pickle('cells_name2.p')):
-    print(cell_name)
-    if cell_name=='2017_04_03_B':continue
-    plot_dict={'color':colors[i],'label':cell_name,'lw':scatter_size-2}
-    W_AMPA=[]
-    j=0
-    dictMOO={'passive_parameter':find_RA(save_folder+cell_name),'syn_num':None,'from_picture':cell_name in read_from_pickle('cells_sec_from_picture.p'),'double_spine_area':False}
-    PSD=get_MOO_result_parameters(cell_name,'PSD',**dictMOO)
-    RA=get_MOO_result_parameters(cell_name,'RA',**dictMOO)
-    W_AMPA=get_MOO_result_parameters(cell_name,'W_AMPA',**dictMOO)
-    W_NMDA=get_MOO_result_parameters(cell_name,'W_NMDA',**dictMOO)
-    V_NMDA=get_MOO_result_parameters(cell_name,'V_syn_NMDA',**dictMOO)
-    distance=get_MOO_result_parameters(cell_name,'distance',**dictMOO)
-    std_AMPA,std_NMDA=get_std_halloffame(cell_name,folder2run=save_folder)
-    if sum(W_NMDA)<=0.005*sum(PSD/max(PSD)):
-        W_NMDA,V_NMDA,std_NMDA=[None]*len(PSD),[None]*len(PSD),[None]*len(PSD)
-    all_AMPA=np.append(all_AMPA, W_AMPA)
-    all_PSD=np.append(all_PSD, PSD)
-    all_std_AMPA=np.append(all_std_AMPA, std_AMPA*np.ones(len(W_AMPA)))#(PSD/max(PSD))/sum(PSD/max(PSD)))
-    all_color=np.append(all_color,[colors[i]]*get_n_spinese(cell_name))
-    if None in W_NMDA:
-        all_std_NMDA=np.append(all_std_NMDA, None)#(PSD/max(PSD))/sum(PSD/max(PSD)))
-    else:
-        all_std_NMDA=np.append(all_std_NMDA, std_NMDA*np.ones(len(W_AMPA)))#(PSD/max(PSD))/sum(PSD/max(PSD)))
-        # ax0_1.errorbar(PSD, W_AMPA, std_AMPA*10, linestyle='None',ecolor=colors[i],elinewidth=scatter_size-4)
-        # ax0_2.errorbar(PSD, W_NMDA, std_NMDA*10, linestyle='None',ecolor=colors[i],elinewidth=scatter_size-4)
-        # ax0_3.errorbar(W_AMPA, W_NMDA,std_NMDA*10, std_AMPA*100, linestyle='None',ecolor=colors[i],elinewidth=scatter_size-6)
-        all_W_NMDA=np.append(all_W_NMDA, W_NMDA)
-        all_V_NMDA=np.append(all_V_NMDA, V_NMDA)
-        all_PSD_NMDA=np.append(all_PSD_NMDA, PSD)
-        all_AMPA_NMDA=np.append(all_AMPA_NMDA, W_AMPA)
+adgust_subplot(ax1,'' ,'PSD [um^2]','g_max AMPA [nS]',latter='A')
+adgust_subplot(ax2,'','PSD [um^2]','g NMDA [nS]',latter='B')
+adgust_subplot(ax3,'','gmax AMPA [nS]','Vmax NMDA [mV]',latter='C')
+dict4plot={'ax1':[all_PSD,all_W_AMPA],'ax2':[all_PSD_NMDA,all_W_NMDA],'ax3':[all_W_AMPA_NMDA,all_V_soma_NMDA]}
+plot_dict={'label':cell_name,'lw':scatter_size-1,'color':all_color}
+for key,item in dict4plot.items():
+    ax=eval(key)
+    ax.scatter(item[0],item[1],**plot_dict)
+    # add_curve_fit(ax,item[0],item[1],start_in_zero=False,plot_curve=False)
 
-    ax0_1.scatter(PSD,W_AMPA,**plot_dict)
-    ax0_2.scatter(PSD,W_NMDA,**plot_dict)
-    ax0_3.scatter(W_AMPA,V_NMDA,**plot_dict)
+ax3.legend(loc='upper right',bbox_to_anchor=(1.2, 0.55),prop={'size': legend_size-1})
 
-
-ax0_3.legend(loc='upper right',bbox_to_anchor=(1.2, 0.55),prop={'size': legend_size-1})
-
-add_curve_fit(ax0_1,all_PSD, all_AMPA,'g_density',name_units='pS/um^2')
-add_curve_fit(ax0_2,all_PSD_NMDA, all_W_NMDA,'g_density',name_units='pS/um^2')
-add_curve_fit(ax0_3,all_AMPA_NMDA, all_V_NMDA,'AMPA/NMDA',name_units='AU',x_m=0.62)
+add_curve_fit(ax1,all_PSD, all_W_AMPA,'g_density',name_units='pS/um^2')
+add_curve_fit(ax2,all_PSD_NMDA, all_W_NMDA_NMDA,'g_density',name_units='pS/um^2')
+add_curve_fit(ax3,all_W_AMPA_NMDA, all_V_NMDA_NMDA,'AMPA/NMDA',name_units='AU',x_m=0.62)
 
 plt.savefig(save_dir+'/AMPA_NMDA_PSD_RA_min_error.png')
 plt.savefig(save_dir+'/AMPA_NMDA_PSD_RA_min_error.svg')
@@ -276,9 +253,11 @@ ax0_1 = plt.subplot2grid(shape=shapes, loc=(0, 0), rowspan=1, colspan=1)
 ax0_2 = plt.subplot2grid(shape=shapes, loc=(0, 1), colspan=1, rowspan=1)
 ax0_3 = plt.subplot2grid(shape=shapes, loc=(0, 2), colspan=1, rowspan=1)
 
-adgust_subplot(ax0_1,'PSD - distance','distance [um]','PSD [um^2]','A')
-adgust_subplot(ax0_2,'AMPA - distance' ,'distance [um]','gmax AMPA [nS]','B')
-adgust_subplot(ax0_3,'NMDA - distance','distance [um]','Vmax NMDA [mV]','C')
+adgust_subplot(ax0_1,'','distance [um]','PSD [um^2]','A')
+adgust_subplot(ax0_2,'' ,'distance [um]','gmax AMPA [nS]','B')
+adgust_subplot(ax0_3,'','distance [um]','Vmax NMDA [mV]','C')
+adgust_subplot(ax0_3,'','PSD/spine_head_area','gmax [mV]','C')
+
 all_AMPA,all_NMDA,all_PSD,all_dis,all_dis_NMDA=[],[],[],[],[]
 for i,cell_name in enumerate(read_from_pickle('cells_name2.p')):
     plot_dict={'color':colors[i],'label':cell_name,'lw':scatter_size-2}
