183cd0b86c1fd5337696503d7a473ad97718c1dc
moriafrid <moria.fridman100@gmail.com>
2022-11-03 14:07:39 +0200

    take shrinkage factor to ve just 1.0,1.0

--- find_MOO_file.py
+++ find_MOO_file.py
@@ -47,20 +47,31 @@ def check_if_continue(model_place,cell_name='None'):
             cont=True
     if 'test' in model_place.split('/')[-1] or 'test' in model_place.split('/')[-2]:
         cont=True
+    if get_n_spinese(cell_name)>1 and 'same' in model_place:
+        cont=True
     return cont
 
-def model2run(specipic_cell='*'):
+def model2run(specipic_cell='*',shrinkage='*'):
     folders=[]
     folder_=''
     if specipic_cell=='*':
         for moo_file in MOO_file(before_after='_before_shrink')+MOO_file(before_after='_after_shrink'):
-            folders+=glob(folder_+'cells_outputs_data_short/'+specipic_cell+'/'+moo_file+'/F_shrinkage=*/const_param/*')
+            folders+=glob(folder_+'cells_outputs_data_short/'+specipic_cell+'/'+moo_file+'/'+shrinkage+'/const_param/*')
     else:
         for moo_file in MOO_file(specipic_cell,before_after='_before_shrink')+MOO_file(specipic_cell,before_after='_after_shrink'):
-            folders+=glob(folder_+'cells_outputs_data_short/'+specipic_cell+'/'+moo_file+'/F_shrinkage=*/const_param/*')
-    file2run=[]
+            folders+=glob(folder_+'cells_outputs_data_short/'+specipic_cell+'/'+moo_file+'/'+shrinkage+'/const_param/*')
+    file2run0=[]
+    if specipic_cell=='*':
+        specipic_cell=None
+
     for model_place in tqdm(folders):
+        model_place=model_place.replace('//','/')
         if check_if_continue(model_place,cell_name=specipic_cell): continue
-        file2run.append(model_place.replace('//','/'))
+        file2run0.append(model_place)
+    file2run=[]
+    for passive_val in ['RA_min_error','RA_best_fit','RA=70','RA=100','RA=120','RA=150','RA=200','RA=300']:
+        for file in file2run0:
+            if passive_val in file:
+                file2run.append(file)
     print('length for model to run is' ,len(file2run))
     return file2run
