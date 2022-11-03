bec025cb5effc623b05d9ef13b6565be61b0de4e
moriafrid <moria.fridman100@gmail.com>
2022-11-03 14:09:40 +0200

    made one figre to be the same as subplot fgure

--- add_figure.py
+++ add_figure.py
@@ -2,17 +2,18 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['svg.fonttype'] = 'none'
-def add_figure(title,x_label,y_label):
+def add_figure(title,x_label,y_label,titlesize=30,bottom_visiability=True,xylabelsize=20,xytitlesize=20):
 	plt.close()
 	figure=plt.figure()
-	plt.rc('font', size=20)          # controls default text sizes
-	plt.rc('axes', titlesize=16)     # fontsize of the axes title
-	plt.rc('axes', labelsize=16)    # fontsize of the x and y labels
-	plt.rc('xtick', labelsize=10)    # fontsize of the tick labels
-	plt.rc('ytick', labelsize=10)    # fontsize of the tick labels
-	plt.rc('legend', fontsize=16)    # legend fontsize
-	plt.rc('figure', titlesize=22)  # fontsize of the figure title
-	plt.title(title,fontsize=15)
+	plt.rc('axes', titlesize=xytitlesize)     # fontsize of the axes title
+	plt.rc('axes', labelsize=xylabelsize)    # fontsize of the x and y labels
+	plt.rc('xtick', labelsize=20)    # fontsize of the tick labels
+	plt.rc('ytick', labelsize=20) #controls default text sizes
+	# plt.rc('axes', titlesize=24)     # fontsize of the axes title
+	# plt.rc('axes', labelsize=24)    # fontsize of the x and y labels
+	plt.rc('xtick', labelsize=20)    # fontsize of the tick labels
+	plt.rc('ytick', labelsize=20)    # fontsize of the tick labels
+	plt.title(title,fontsize=titlesize)
 	plt.xlabel(x_label)
 	plt.ylabel(y_label)
 	import seaborn as sns
@@ -21,7 +22,7 @@ def add_figure(title,x_label,y_label):
 	return figure
 
 
-def adgust_subplot(ax,title,x_label,y_label,latter='',titlesize=30,bottom_visiability=True,xylabelsize=20,xytitlesize=20):
+def adgust_subplot(ax,title,x_label,y_label,latter='',titlesize=30,bottom_visiability=True,xylabelsize=20,xytitlesize=20,e_label=True):
 	ax.text(-0.2, 1.05, latter, transform=ax.transAxes, size=22,weight="bold")
 	# ax.text(-0.1, 0.9, string.ascii_uppercase[i], transform=ax.transAxes, size=25)
 	# plt.rc('font', size=20)
@@ -36,6 +37,8 @@ def adgust_subplot(ax,title,x_label,y_label,latter='',titlesize=30,bottom_visiab
 
 	#plt.rc('legend', fontsize=30)    # legend fontsize
 	# plt.rc('figure', titlesize=40)  # fontsize of the figure title
+	if e_label==True:
+		ax.ticklabel_format(style='sci',scilimits=(0,0),axis='y')
 
 	ax.spines['top'].set_visible(False)
 	ax.spines['right'].set_visible(False)
