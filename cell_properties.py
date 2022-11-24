abecca6fea2c281fdd80c24af0ebadae0231e1c8
moriafrid <moria.fridman100@gmail.com>
2022-11-24 16:25:59 +0200

    run prefectly, the syn is adding

--- cell_properties.py
+++ cell_properties.py
@@ -3,12 +3,15 @@ from neuron import h, gui
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-from add_figure import add_figure
+from add_figure import add_figure, adgust_subplot
 from glob import glob
 from extra_function import create_folder_dirr,SIGSEGV_signal_arises,load_ASC,load_hoc,load_swc
 import sys
 import pickle
 import matplotlib
+
+from read_spine_properties import get_sec_and_seg
+
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['svg.fonttype'] = 'none'
 
@@ -17,7 +20,7 @@ print(len(sys.argv),sys.argv,flush=True)
 if len(sys.argv) != 3:
     print("sys.argv not running and with length",len(sys.argv))
     cell_name= '2017_04_03_B'
-    file_type2read= 'z_correct_after*.swc'
+    file_type2read= 'z_correct_after_shrink.swc'
 else:
     print("sys.argv is correct and running")
     cell_name = sys.argv[1]
@@ -26,11 +29,6 @@ folder_=''
 data_dir= "cells_initial_information"
 save_dir = "cells_outputs_data_short"
 print(cell_name, folder_+data_dir+"/"+cell_name+"/"+file_type2read)
-cell_file = glob(folder_+data_dir+"/"+cell_name+"/*"+file_type2read)[0]
-if 'XYZ' not in file_type2read and 'XYZ' in cell_file:
-    cell_file = glob(folder_+data_dir+"/"+cell_name+"/*"+file_type2read)[1]
-
-print(cell_file,file_type2read)
 
 path_short_pulse=folder_+save_dir+'/'+cell_name+'/data/electrophysio_records/short_pulse/mean_short_pulse.p'
 # folder_save=folder_+save_dir+'/'+cell_name+'/data/cell_properties/'+file_type2read+'SPINE_START=20/dend*'++'&F_shrinkage'++'/diam_dis/'
@@ -42,11 +40,11 @@ signal.signal(signal.SIGSEGV, SIGSEGV_signal_arises)
 
 f=open(folder_save+'cell_propertis.txt', 'w')
 f.write('The '+cell_name+ ' cell_propertis\n')
-def cumpute_distances(base_sec):
+def cumpute_distances(base_sec,cell):
     for sec in h.SectionRef(sec=base_sec).child:
-        add_sec(sec)
-        cumpute_distances(sec)
-def add_sec(sec):
+        add_sec(sec,cell)
+        cumpute_distances(sec,cell)
+def add_sec(sec,cell):
     """
     morpho dendogram
     :param sec:
@@ -56,100 +54,127 @@ def add_sec(sec):
     dist[sec] = h.distance(1, sec=sec)
 
 #track from the terminals to the soma
-def track_one(terminal):
+def track_one(terminal,cell,ax):
     # h.distance(0, 0.5, sec=soma)
     sec=terminal
     dis=[]
     diam=[]
-    while sec !=soma:
+    syn_secs,syn_segs=get_sec_and_seg(cell_name)
+    while sec !=cell.soma:
         # dis.append(h.distance(sec.parentseg()))
-        h.distance(0, 0.5, sec=soma)
+        h.distance(0, 0.5, sec=cell.soma)
 
         dis.append(h.distance(1,sec=sec))
+
         sec_ref=h.SectionRef(sec=sec)
         diam.append(sec.diam)
+        sec_name=sec.name().split('.')[-1]
+        if sec_name in syn_secs:
+            #ax.scatter(sec.diam,dis[-2]+(dis[-1]-dis[-2])*syn_segs[syn_secs.index(sec_name)])
+            syn_dis.append(h.distance(syn_segs[syn_secs.index(sec_name)],sec=sec))
+            syn_diam.append(sec(syn_segs[0]).diam)
+            dis.append(h.distance(1,sec=sec))
+            diam.append(sec.diam)
         sec=sec_ref.parent
     return np.array(dis),np.array(diam)
 ######################################################
 # build the model
 ######################################################
 # h.load_file("/ems/elsc-labs/segev-i/moria.fridman/project/analysis_groger_cells/cells_initial_information/2017_03_04_A_6-7(0)(0)/03_24_A_11052017_Splice_Shrink_zvalue_LABEL_Bluecell_Zcorrected_by_Gregor.hoc")
-if 'ASC' in file_type2read:
-    cell=load_ASC(cell_file)
-elif 'hoc'in file_type2read:
-    cell=load_hoc(cell_file)
-elif 'swc' in file_type2read:
-    cell=load_swc(cell_file)
-print (cell)
-sp = h.PlotShape()
-sp.show(0)  # show diameters
-try:
-    for sec in cell.axon:
-        h.delete_section(sec=sec)
-except:
-    print(cell_name.split('/')[-1] +' dont have axon inside')
-soma= cell.soma
-# insert pas to all other section
-for sec in tqdm(cell.all_sec()):
-    sec.insert('pas') # insert passive property
-    sec.nseg = int(sec.L/10)+1  #decide that the number of segment will be 21 with the same distances
+def diam_distance_plot(cell_name,file_type2read,data_dir= "cells_initial_information",ax=None,latter=''):
+    global syn_dis,syn_diam
+    syn_dis,syn_diam=[],[]
+    if ax==None:
+        fig=add_figure('diam-dis'+cell_name+' \n'+file_type2read,'distance from soma','diameter')
+        ax=fig.axes[0]
+    else:
+        adgust_subplot(ax,cell_name,'distance from soma','diameter',latter=latter)
+    cell_file = glob(data_dir+"/"+cell_name+"/*"+file_type2read)[0]
+    if 'XYZ' not in file_type2read and 'XYZ' in cell_file:
+        cell_file = glob(folder_+data_dir+"/"+cell_name+"/*"+file_type2read)[1]
+    if 'ASC' in file_type2read:
+        cell=load_ASC(cell_file)
+    elif 'hoc'in file_type2read:
+        cell=load_hoc(cell_file)
+    elif 'swc' in file_type2read:
+        cell=load_swc(cell_file)
+
+    sp = h.PlotShape()
+    sp.show(0)  # show diameters
+    try:
+        for sec in cell.axon:
+            h.delete_section(sec=sec)
+    except:
+        print(cell_name.split('/')[-1] +' dont have axon inside')
+    soma= cell.soma
+    # insert pas to all other section
+    for sec in tqdm(cell.all_sec()):
+        sec.insert('pas') # insert passive property
+        sec.nseg = int(sec.L/10)+1  #decide that the number of segment will be 21 with the same distances
 
 
-clamp = h.IClamp(soma(0.5)) # insert clamp(constant potentientiol) at the soma's center
-clamp.amp = -0.05  ## supopsed to be 0.05nA
-clamp.dur = 200
-clamp.delay = 190
+    clamp = h.IClamp(soma(0.5)) # insert clamp(constant potentientiol) at the soma's center
+    clamp.amp = -0.05  ## supopsed to be 0.05nA
+    clamp.dur = 200
+    clamp.delay = 190
 
-#short_pulse, hz, rest = read_from_pickle(path, hz=True ,rest=True)
-# V = short_pulse[0]
-# T = short_pulse[1]
-# E_PAS = rest
-# V+=E_PAS
-#
-# h.tstop = T[-1]
-# h.v_init=E_PAS
-# h.dt = 0.1
-# h.steps_per_ms = h.dt
+    #short_pulse, hz, rest = read_from_pickle(path, hz=True ,rest=True)
+    # V = short_pulse[0]
+    # T = short_pulse[1]
+    # E_PAS = rest
+    # V+=E_PAS
+    #
+    # h.tstop = T[-1]
+    # h.v_init=E_PAS
+    # h.dt = 0.1
+    # h.steps_per_ms = h.dt
 
-imp = h.Impedance(sec=soma)
-imp.loc(soma(0.5))
-imp.compute(0)
-imp.input(0)
-imp.compute(0)
-print('the impadence is',imp.input(0))
-f.write('The impadence is '+str(imp.input(0))+'\n')
+    imp = h.Impedance(sec=soma)
+    imp.loc(soma(0.5))
+    imp.compute(0)
+    imp.input(0)
+    imp.compute(0)
+    print('the impadence is',imp.input(0))
+    f.write('The impadence is '+str(imp.input(0))+'\n')
 
-#print the dendrite diameter:
-soma_ref=h.SectionRef(sec=soma)
-print("the soma's childrens diameter is:")
-f.write("\nThe soma's childrens diameter is\n")
+    #print the dendrite diameter:
+    soma_ref=h.SectionRef(sec=soma)
+    print("the soma's childrens diameter is:")
+    f.write("\nThe soma's childrens diameter is\n")
 
-for i in range(soma_ref.nchild()):
-    print(soma_ref.child[i](0).diam)
-    f.write(str(soma_ref.child[i](0).diam)+"\n")
+    for i in range(soma_ref.nchild()):
+        print(soma_ref.child[i](0).diam)
+        f.write(str(soma_ref.child[i](0).diam)+"\n")
 
-length=0
-for dend in cell.dend:
-    length+=dend.L
-print("total dendritic length is ",length)
-f.write("\nThe total dendritic length is "+str(length)+ "\n")
-f.close()
+    length=0
+    for dend in cell.dend:
+        length+=dend.L
+    print("total dendritic length is ",length)
+    ax.text(-0.1, 1.1,'Total L='+str(round(length))+'um',transform=ax.transAxes,size=20)
+    f.write("\nThe total dendritic length is "+str(length)+ "\n")
+    f.close()
 
-terminals = []
-for sec in cell.dend:
-    if len(sec.children())==0:
-        terminals.append(sec)
-plt.close()
-fig=add_figure('diam-dis relation along dendrites with diffrent collors\n'+cell_name+' '+file_type2read,'distance from soma','diameter')
-i=0
-for terminal in terminals:
-    i+=1
-    dis,diam=track_one(terminal)
-    plt.plot(dis,diam,alpha=0.5)
-    if len(diam)==1:
-        plt.plot(dis,diam,'*')
-plt.show()
-plt.savefig(folder_save+'diam-dis.png')
-plt.savefig(folder_save+'diam-dis.pdf')
-pickle.dump(fig, open(folder_save+'diam-dis.p', 'wb'))
-plt.show()
+    terminals = []
+    for sec in cell.all_sec():
+        if len(sec.children())==0:
+            terminals.append(sec)
+
+
+    i=0
+    for terminal in terminals:
+        i+=1
+        dis,diam=track_one(terminal,cell,ax)
+        ax.plot(dis,diam,alpha=0.5)
+        if len(diam)==1:
+            ax.plot(dis,diam,'*')
+    i=0
+    for syn_dis1,syn_diam1 in zip(syn_dis,syn_diam):
+        ax.scatter(syn_dis1,syn_diam1,label='syn'+str(i)+'; dis='+str(round(syn_dis1,2))+'um')
+        i+=1
+    plt.legend()
+    plt.savefig(folder_save+'diam-dis.png')
+    plt.savefig(folder_save+'diam-dis.pdf')
+    pickle.dump(fig, open(folder_save+'diam-dis.p', 'wb'))
+    plt.show()
+if __name__=="__main__":
+    diam_distance_plot(cell_name,file_type2read,data_dir= "cells_initial_information")
--- check_dynamics.py
+++ check_dynamics.py
@@ -5,32 +5,38 @@ import matplotlib
 from extra_fit_func import short_pulse_edges
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['svg.fonttype'] = 'none'
-def check_dynamics(short_pulse,x_short_pulse,save_folder):
+def check_dynamics(short_pulse,x_short_pulse,save_folder,ax=None):
     cell_name=save_folder.split('/')[1]
     add_figure('short_pulse','ms','mV')
     plt.plot(x_short_pulse,short_pulse,'.')
     plt.savefig(save_folder+'/the short pulse')
+    plt.close()
     print("Done {0}".format(save_folder+'/short_pulse'))
-    fig=add_figure('cell_name','s','mV')
+    cell_name=save_folder.split('/')[1]
+    if ax is None:
+        fig=add_figure('','s','mV')
+        fig.set_size_inches(5,5)
+        fig.subplots_adjust(left=0.2,right=0.95,top=0.9,bottom=0.15)
+        ax=fig.axes[0]
     short_pulse_start,short_pulse_end,length=short_pulse_edges(cell_name)
 
     part1=short_pulse[short_pulse_start:short_pulse_end]-short_pulse[0]
     part2=-short_pulse[short_pulse_end:]+short_pulse[short_pulse_end]
     x_part1=x_short_pulse[short_pulse_start:short_pulse_end]
     x_part1-=x_part1[0]
-    plt.plot(x_part1*1000, part1,label='beginigng')
+    ax.plot(x_part1*1000, part1,label='beginigng')
     if len(part2)<len(part1):
-        plt.plot(x_part1[:len(part2)]*1000,part2[:len(x_part1)],label='flip end')
+        ax.plot(x_part1[:len(part2)]*1000,part2[:len(x_part1)],label='flip end')
     else:
-        plt.plot(x_part1*1000,part2[:len(x_part1)],label='flip end')
+        ax.plot(x_part1*1000,part2[:len(x_part1)],label='flip end')
 
-    plt.legend()
+    ax.legend()
     plt.savefig(save_folder+'/check_dynamics.png')
-    plt.savefig(save_folder+'/check_dynamics.pdf')
+    plt.savefig(save_folder+'/check_dynamics.svg')
     pickle.dump(fig, open(save_folder+'/check_dynamics.p', 'wb'))
 
     print("Done {0}".format(save_folder+'/short_pulse'))
-plt.show()
+
 
 
 
